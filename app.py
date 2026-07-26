a763c9664eddbf3f9600fdad8d4860e3a065c967
Rutwik-M <rmuley558@gmail.com>
2026-07-26 11:34:38 +0530

    Fix: Point to favicons in static folder and span footer across the website

--- app.py
+++
@@ -1,8 +0,0 @@
-cgpa = float(input("Enter your CGPA: "))
-
-if cgpa > 0:
-    percentage = (cgpa - 0.75)*10 
-    print(f"Your equivalent percentage for the CGPA are:")
-    print(f"{percentage} %")
-else:
-    print("CGPA cannot be negative")
